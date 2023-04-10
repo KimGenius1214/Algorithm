a9719590c8389db95062dd7d74b758da737d7bd7
김영재 <dzdiz45@gmail.com>
2023-04-10 09:54:13 +0900

    230410

---
+++ 프로그래머스/solution.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+  my_string = "Bcad";
+  var answer = "";
+  let newAnswer = my_string.toLowerCase();
+  answer = splitAndSort(newAnswer);
+  return answer;
+}
+
+function splitAndSort(str) {
+  return str.split("").sort().join("");
+}
+
+solution();
