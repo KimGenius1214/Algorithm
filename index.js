7765d55492a36c3cfb3886e87f871c3665bbb619
김영재 <dzdiz45@gmail.com>
2023-04-13 13:25:57 +0900

    230413

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
 // 5585 거스름돈
 const fs = require("fs");
-// const input = fs.readFileSync("/dev/stdin").toString();
-const input = fs.readFileSync("example.txt").toString();
-let a = Number(input);
-
-const solution = (a) => {
-  let result = 0;
-  let deposit = 0;
-  while (true) {
-    if (deposit + a === 1000) {
-      console.log(result);
-      break;
-    }
-    if (a > 500) {
-      result += 1;
-      deposit += 500;
-    } else if (a < 500 && a >= 100) {
-      result += 1;
-      deposit += 100;
-    } else if (a < 100 && a >= 50) {
-      result += 1;
-      deposit += 50;
-    } else if (a < 50 && a >= 10) {
-      result += 1;
-      deposit += 10;
-    } else if (a < 10 && a >= 5) {
-      result += 1;
-      deposit += 5;
-    } else if (a < 5 && a >= 1) {
-      result += 1;
-      deposit += 1;
-    }
-  }
-};
-solution(a);
+const input = fs.readFileSync("/dev/stdin").toString();
+// const input = fs.readFileSync("example.txt").toString();
+let change = 1000 - input;
+let count = 0;
+const coins = [500, 100, 50, 10, 5, 1];
+for (let i = 0; i < coins.length; i++) {
+  let quo = Math.floor(change / coins[i]);
+  change %= coins[i];
+  count += quo;
+}
+console.log(count);
--- 백준알고리즘/5585.js
+++ 백준알고리즘/5585.js
@@ -1,32 +1,13 @@
 // 5585 거스름돈
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString();
-// const input = fs.readFileSync("example.txt").toString()
-let a = Number(input);
-
-const solution = (a) => {
-  let result = 0;
-  while (a === 0) {
-    if (a < 500) {
-      result += 1;
-      a -= 500;
-    } else if (a < 100) {
-      result += 1;
-      a -= 100;
-    } else if (a < 50) {
-      result += 1;
-      a -= 50;
-    } else if (a < 10) {
-      result += 1;
-      a -= 10;
-    } else if (a < 5) {
-      result += 1;
-      a -= 5;
-    } else if (a < 1) {
-      result += 1;
-      a -= 1;
-    }
-  }
-  return result;
-};
-solution(a);
+// const input = fs.readFileSync("example.txt").toString();
+let change = 1000 - input;
+let count = 0;
+const coins = [500, 100, 50, 10, 5, 1];
+for (let i = 0; i < coins.length; i++) {
+  let quo = Math.floor(change / coins[i]);
+  change %= coins[i];
+  count += quo;
+}
+console.log(count);
