587b8c7f28eddf1852b4e2b519d485cafa9f17c2
김영재 <dzdiz45@gmail.com>
2023-04-18 10:11:38 +0900

    230418

--- 백준알고리즘/10870.js
+++ 백준알고리즘/10870.js
@@ -1,15 +1,13 @@
 // 10870 피보나치 수 5
 const fs = require("fs");
-// const input = fs.readFileSync("example.txt").toString();
-var input = fs.readFileSync("/dev/stdin").toString();
-
+const input = fs.readFileSync("example.txt").toString();
 let n = Number(input);
+// var input = fs.readFileSync("/dev/stdin").toString();
 let prev = 0;
 let next = 1;
 let result = 0;
 for (let i = 0; i < n; i++) {
-  result = prev + next;
+  next = prev + next;
   prev = next;
-  next = result;
 }
 console.log(prev);
