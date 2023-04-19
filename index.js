88c8e53faa0756cc8dca0a2dc70ab2c4e6090680
김영재 <dzdiz45@gmail.com>
2023-04-19 13:29:01 +0900

    230419

--- index.js
+++ index.js
@@ -24,4 +24,16 @@ let getGCD = (num1, num2) => {
   return gcd;
 };
 
+let getLCM = (num1, num2) => {
+  let lcm = 1;
+
+  while (true) {
+    if (lcm % num1 == 0 && lcm % num2 == 0) {
+      break;
+    }
+    lcm++;
+  }
+  return lcm;
+};
+
 solution(a, b);
