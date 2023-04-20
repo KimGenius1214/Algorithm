ab307badd0501963b38f09bc585d14fb06f97c8a
김영재 <dzdiz45@gmail.com>
2023-04-20 10:35:41 +0900

    230420

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-const arr = [1, 1, 3, 3, 0, 1, 1];
+const arr = [4, 4, 4, 3, 3];
 function solution(arr) {
   var answer = [];
-  const newSet = new Set(arr);
-
-  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-  console.log(Array(newSet));
-
-  return answer;
+  arr.forEach((e) => {
+    if (answer.length > 0 && answer[answer.length - 1] !== e) {
+      answer.push(e);
+    } else if (answer.length === 0) answer.push(arr[0]);
+  });
+  console.log(answer);
 }
 
 solution(arr);
--- 프로그래머스/lv1_같은 숫자는 싫어.js
+++ 프로그래머스/lv1_같은 숫자는 싫어.js
@@ -1,9 +1,12 @@
+const arr = [4, 4, 4, 3, 3];
 function solution(arr) {
   var answer = [];
-  const newSet = Set(arr);
-
-  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-  console.log(newSet);
-
-  return answer;
+  arr.forEach((e) => {
+    if (answer.length > 0 && answer[answer.length - 1] !== e) {
+      answer.push(e);
+    } else if (answer.length === 0) answer.push(arr[0]);
+  });
+  console.log(answer);
 }
+
+solution(arr);
