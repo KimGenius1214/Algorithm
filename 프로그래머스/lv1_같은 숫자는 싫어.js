776737d17abb06a617abb4d35bb406edeab7e348
김영재 <dzdiz45@gmail.com>
2023-04-20 10:20:31 +0900

    230420

--- 프로그래머스/lv1_같은 숫자는 싫어.js
+++ 프로그래머스/lv1_같은 숫자는 싫어.js
@@ -1,12 +1,9 @@
-const arr = [1, 1, 3, 3, 0, 1, 1];
 function solution(arr) {
   var answer = [];
-  const newSet = new Set(arr);
+  const newSet = Set(arr);
 
   // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-  console.log(Array(newSet));
+  console.log(newSet);
 
   return answer;
 }
-
-solution(arr);
