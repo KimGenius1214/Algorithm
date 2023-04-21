c5316378d0522fa981cb1279a88c5d5fa78f2d9d
김영재 <dzdiz45@gmail.com>
2023-04-21 10:05:57 +0900

    230421

--- 백준알고리즘/10988.js
+++ 백준알고리즘/10988.js
@@ -1,10 +1,7 @@
 // 10988 팰린드롬인지 확인하기
 const fs = require("fs");
-// const input = fs
-//   .readFileSync("/dev/stdin")
-//   .toString()
-//   .trim()
-const input = fs.readFileSync("example.txt").toString().trim();
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+// const input = fs.readFileSync("example.txt").toString().trim();
 const reverseInput = input.split("").reverse().join("");
 if (input === reverseInput) {
   console.log(1);
