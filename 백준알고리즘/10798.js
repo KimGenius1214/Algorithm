310947c921681c41abce6798bc103164d2c6a356
김영재 <dzdiz45@gmail.com>
2023-04-25 10:45:56 +0900

    230425

--- 백준알고리즘/10798.js
+++ 백준알고리즘/10798.js
@@ -1,9 +1,30 @@
 // 10798 세로읽기
 const fs = require("fs");
 const input = fs
-  //   .readFileSync("/dev/stdin")
-  .readFileSync("example.txt")
+  .readFileSync("/dev/stdin")
+  // .readFileSync("example.txt")
   .toString()
   .trim()
   .split("\n");
-console.log(input);
+let arr = [];
+let num = 0;
+while (true) {
+  let tempArr = [];
+  for (const i of input) {
+    if (i[num] !== undefined && i[num] !== "\r") {
+      arr.push(i[num]);
+    }
+    tempArr.push(i[num]);
+  }
+  if (
+    tempArr[0] === undefined &&
+    tempArr[1] === undefined &&
+    tempArr[2] === undefined &&
+    tempArr[3] === undefined &&
+    tempArr[4] === undefined
+  ) {
+    break;
+  }
+  num++;
+}
+console.log(arr.join(""));
