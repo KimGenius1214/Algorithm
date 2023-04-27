73a42948579101d4b946822e1cd5bcc098fe38d6
김영재 <dzdiz45@gmail.com>
2023-04-27 10:30:10 +0900

    230427

--- 백준알고리즘/1934.js
+++ 백준알고리즘/1934.js
@@ -11,24 +11,13 @@ const a = Number(input[0]);
 const answer = [];
 let getGCD = (num1, num2) => {
   let gcd = 1;
-
   for (let i = 2; i <= Math.min(num1, num2); i++) {
     if (num1 % i === 0 && num2 % i === 0) {
       gcd = i;
     }
   }
-
   return gcd;
 };
-
-for (let i = 1; i <= a; i++) {
-  let x = Number(input[i].split(" ")[0]);
-  let y = Number(input[i].split(" ")[1]);
-  let common = getGCD(x, y);
-  answer.push((x * y) / common);
-}
-
-console.log(answer.join("\n").trim());
 
 // 최소공배수
 function getLCM(a, b) {
@@ -41,3 +30,12 @@ function getLCM(a, b) {
   }
   return lcm;
 }
+
+for (let i = 1; i <= a; i++) {
+  let x = Number(input[i].split(" ")[0]);
+  let y = Number(input[i].split(" ")[1]);
+  let common = getGCD(x, y);
+  answer.push((x * y) / common);
+}
+
+console.log(answer.join("\n").trim());
