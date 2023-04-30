6e02bb883652a49d5cad7100f9393adb6047e811
김영재 <dzdiz45@gmail.com>
2023-04-30 12:50:03 +0900

    230430

--- 백준알고리즘/1427.js
+++ 백준알고리즘/1427.js
@@ -1,8 +1,8 @@
 // 1427 소트인사이드
 const fs = require("fs");
 const input = fs
-  .readFileSync("/dev/stdin")
-  // .readFileSync("example.txt")
+  //   .readFileSync("/dev/stdin")
+  .readFileSync("example.txt")
   .toString()
   .trim();
 
