fd07c91d0aacc5fef4bfc87e2a134d0c97972593
김영재 <dzdiz45@gmail.com>
2023-05-05 21:20:44 +0900

    230505

--- 프로그래머스/lv1_문자열을 정수로 바꾸기.js
+++ 프로그래머스/lv1_문자열을 정수로 바꾸기.js
@@ -1,6 +1,11 @@
 const s = "-1234";
 function solution(s) {
   var answer = Number(s);
+  let sign = 1;
+  //   if (s[0] === "-") {
+  //     sign = -1
+  //   }
+
   return answer;
 }
 solution(s);
