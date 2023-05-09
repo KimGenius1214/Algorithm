3a069e084fa6235724f3b89e2ba5641c1df2dce2
김영재 <dzdiz45@gmail.com>
2023-05-09 09:37:05 +0900

    230509

--- 프로그래머스/lv1_서울에서 김서방 찾기.js
+++ 프로그래머스/lv1_서울에서 김서방 찾기.js
@@ -1,6 +1,7 @@
 const seoul = ["Jane", "Kim"];
 function solution(seoul) {
   var answer = seoul.indexOf("Kim");
+  console.log(`김서방은 ${answer}에 있다`);
   return `김서방은 ${answer}에 있다`;
 }
 solution(seoul);
