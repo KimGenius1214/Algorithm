6a5c35fa5fb784beddad95171b8eceb0cc61e4d9
김영재 <dzdiz45@gmail.com>
2023-05-15 10:17:48 +0900

    230515

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 // 10814 나이순 정렬
 const fs = require("fs");
 const input = fs
-  //   .readFileSync("/dev/stdin")
-  .readFileSync("example.txt")
+  .readFileSync("/dev/stdin")
+  // .readFileSync("example.txt")
   .toString()
   .trim()
   .split("\n");
 const a = input.shift();
-let arr = [];
-for (const i of input) {
-  arr.push(i.split(" "));
-}
-arr.map((r) => {
-  console.log();
-});
+input.sort((a, b) => a.split(" ")[0] - b.split(" ")[0]);
+console.log(input.join("\n"));
--- 백준알고리즘/10814.js
+++ 백준알고리즘/10814.js
@@ -1,17 +1,11 @@
 // 10814 나이순 정렬
 const fs = require("fs");
 const input = fs
-  //   .readFileSync("/dev/stdin")
-  .readFileSync("example.txt")
+  .readFileSync("/dev/stdin")
+  // .readFileSync("example.txt")
   .toString()
   .trim()
   .split("\n");
 const a = input.shift();
-let arr = [];
-for (const i of input) {
-  arr.push(i.split(" "));
-}
-console.log(arr);
-for (const i of arr) {
-  console.log(i[0]);
-}
+input.sort((a, b) => a.split(" ")[0] - b.split(" ")[0]);
+console.log(input.join("\n"));
