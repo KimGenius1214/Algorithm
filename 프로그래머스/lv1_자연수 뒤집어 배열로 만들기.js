27030d69ecf3b7e63272edb77211c17063db7ee9
김영재 <dzdiz45@gmail.com>
2023-05-23 09:48:26 +0900

    230523

--- 프로그래머스/lv1_자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/lv1_자연수 뒤집어 배열로 만들기.js
@@ -1,16 +1,15 @@
-const n = 435335;
+const n = 12345;
 
 function solution(n) {
   let answer = [];
   for (const i of String(n)) {
     answer.push(i);
   }
-  let res = answer.join("").split("").reverse();
+  let res = answer.sort((a, b) => Number(b) - Number(a));
   let newRes = [];
   for (const i of res) {
     newRes.push(Number(i));
   }
-  console.log(newRes);
   return newRes;
 }
 
