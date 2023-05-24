28e3580e1e6ab17cc5c92126515466f814237d44
김영재 <dzdiz45@gmail.com>
2023-05-24 10:29:00 +0900

    230524

--- 백준알고리즘/2941.js
+++ 백준알고리즘/2941.js
@@ -1,10 +1,21 @@
 // 2941 크로아티아 알파벳
 const fs = require("fs");
-const input = fs
+let input = fs
   .readFileSync("/dev/stdin")
   // .readFileSync("example.txt")
   .toString()
   .trim();
 
-const dic = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
-console.log(input.includes(dic));
+function solution(input) {
+  const dic = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+  let num = 0;
+  for (let i = 0; i < dic.length; i++) {
+    while (input !== input.replace(dic[i], "")) {
+      num += 1;
+      input = input.replace(dic[i], " ");
+    }
+  }
+  return num + input.split(" ").join("").length;
+}
+
+console.log(solution(input));
