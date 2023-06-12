b34719b429274fcbca95c44228cec5c5329aa1d2
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2023-06-12 09:47:57 +0900

    230612

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 // 17608 막대기
 const input = require("fs")
-  // .readFileSync("/dev/stdin", "utf-8")
-  .readFileSync("example.txt", "utf-8")
-  .trim()
-  .split("\n")
-  .map(Number);
+  .readFileSync("/dev/stdin", "utf-8")
+  // .readFileSync("example.txt", "utf-8")
+  .trim();
 
-const a = input.pop();
-const b = input.shift();
-let num = 1;
-
-for (const i of input) {
-  if (i > a) {
-    num += 1;
-  }
-}
-console.log(num);
+console.log(parseInt(input, 16));
