803085f2d0f284897dfb8e0f39a32ca8d17a2521
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2023-06-12 09:45:51 +0900

    230612

--- 백준알고리즘/17608.js
+++ 백준알고리즘/17608.js
@@ -1,13 +1,10 @@
 // 17608 막대기
 const input = require("fs")
-  // .readFileSync("/dev/stdin", "utf-8")
-  .readFileSync("example.txt", "utf-8")
+  .readFileSync("/dev/stdin", "utf-8")
   .trim()
-  .split("\n")
-  .map(Number);
+  .split("\n");
 
 const a = input.pop();
-const b = input.shift();
 let num = 1;
 
 for (const i of input) {
