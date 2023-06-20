49546c3bc1410b81cf187b6037869ca5b0d1baf3
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2023-06-20 20:52:59 +0900

    230620

--- 프로그래머스/lv1_폰켓몬.js
+++ 프로그래머스/lv1_폰켓몬.js
@@ -1,14 +1,14 @@
-const nums = [3, 3, 3, 2, 2, 2];
+const nums = [3, 1, 2, 3];
 function solution(nums) {
   var answer = 0;
   const numsSet = new Set(nums);
   const len = numsSet.size;
   if (nums.length / 2 > len) {
-    answer = nums.length / 2 - 1;
-  } else {
     answer = len;
+  } else {
+    answer = nums.length / 2;
   }
-  console.log(len);
+  console.log(answer);
   return answer;
 }
 solution(nums);
