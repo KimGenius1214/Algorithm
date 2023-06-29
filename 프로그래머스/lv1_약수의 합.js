d105cc2c5636b1f0d960617746cc94bc973e4827
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2023-06-29 09:26:04 +0900

    230629

---
+++ 프로그래머스/lv1_약수의 합.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  const divisors = [];
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      divisors.push(i);
+      if (n / i != i) divisors.push(n / i);
+    }
+  }
+
+  // divisors.sort((a, b) => a - b);
+  return divisors.reduce((acc, cur) => acc + cur, 0);
+  // return divisors;
+}
