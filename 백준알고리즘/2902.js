be3b2d583326f985e0529b999e5be8c1e7888864
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2023-08-04 09:34:50 +0900

    230804

--- 백준알고리즘/2902.js
+++ 백준알고리즘/2902.js
@@ -1,8 +1,8 @@
 // 2902 KMP는 왜 KMP일까?
 const fs = require("fs");
 const input = fs
-  //   .readFileSync("/dev/stdin")
-  .readFileSync("example.txt")
+  .readFileSync("/dev/stdin")
+  //   .readFileSync("example.txt")
   .toString()
   .trim();
 
