cc7c7e4800e8b01371a73e9fa0c15a46df156988
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2023-08-28 09:37:20 +0900

    230828

--- 백준알고리즘/25238.js
+++ 백준알고리즘/25238.js
@@ -1,14 +1,15 @@
 // 25238 가희와 방어율 무시
 const fs = require("fs");
 const input = fs
-  // .readFileSync("/dev/stdin")
-  .readFileSync("example.txt")
+  .readFileSync("/dev/stdin")
+  //   .readFileSync("example.txt")
   .toString()
   .trim()
   .split(" ");
 
 const a = Number(input[0]);
 const b = Number(input[1]);
+
 const result = a - a * (b / 100);
 
 if (result >= 100) {
