2503296b091057f7e3c8c2c3ca682a6b45353fd7
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2023-08-29 09:03:53 +0900

    230829

--- 프로그래머스/lv1_모의고사.js
+++ 프로그래머스/lv1_모의고사.js
@@ -1,8 +1,22 @@
-const answers = [1, 2, 3, 4, 5];
 function solution(answers) {
-  var answer = [];
-  let arr1 = Array(answers.length).fill([1, 2, 3, 4, 5]);
-  console.log(arr1);
-  return answer;
+  let one = "12345"
+    .repeat(answers.length)
+    .split("")
+    .map((v) => v * 1);
+  let two = "21232425"
+    .repeat(answers.length)
+    .split("")
+    .map((v) => v * 1);
+  let three = "3311224455"
+    .repeat(answers.length)
+    .split("")
+    .map((v) => v * 1);
+
+  let corr1 = answers.filter((v, i) => v === one[i]).length;
+  let corr2 = answers.filter((v, i) => v === two[i]).length;
+  let corr3 = answers.filter((v, i) => v === three[i]).length;
+
+  let correct = [corr1, corr2, corr3];
+
+  return [1, 2, 3].filter((v, i) => correct[i] === Math.max(...correct));
 }
-solution(answers);
