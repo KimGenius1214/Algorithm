eb966450091df8e098d96fa44017b0fd263f34e0
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2023-10-19 11:16:42 +0900

    231019

--- 프로그래머스/lv1_나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/lv1_나머지가 1이 되는 수 찾기.js
@@ -1,5 +1,8 @@
 function solution(n) {
-  for (let i = 2; i < n; i++) {
-    if (n % i === 1) return i;
-  }
+  //   for (let i = 2; i < n; i++) {
+  //     if (n % i === 1) return i;
+  //   }
+  let answer = 1;
+  while (n % answer !== 1) answer++;
+  return answer;
 }
