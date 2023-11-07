18dea0fb1ae15a0b7b600b0c186ece9e658846be
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2023-11-07 09:30:38 +0900

    231107

--- 백준알고리즘/30087.js
+++ 백준알고리즘/30087.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const input = fs
-  .readFileSync("/dev/stdin")
-  // .readFileSync("example.txt")
+  //   .readFileSync("/dev/stdin")
+  .readFileSync("example.txt")
   .toString()
   .trim()
   .split("\n");
