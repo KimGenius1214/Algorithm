206b3a994d398c98a990603707f85de955e13a6f
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2024-03-04 09:37:51 +0900

    240304

--- 코테/hashTable.js
+++ 코테/hashTable.js
@@ -14,6 +14,7 @@ function solution(genres, plays) {
 
   return [...genreMap.entries()]
     .sort((a, b) => b[1].total - a[1].total)
+
     .flatMap((item) => item[1].songs)
     .map((song) => song.index);
 }
