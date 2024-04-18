50c1d46080e97fd5a31487100dd93699eb965691
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2024-04-18 09:54:05 +0900

    240418

---
+++ 프로그래머스/lv0_숨어있는 숫자의 덧셈(2).js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+  var answer = 0;
+  let newAnswer = my_string.replace(/[A-z]/g, " ").split(" ");
+  for (const i of newAnswer) {
+    if (!isNaN(i)) {
+      answer += Number(i);
+    }
+  }
+  return answer;
+}
+s;
