01eb33f1b62948b4f0c01e45ba04431508326f49
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2024-05-01 20:41:45 +0900

    240501

--- 프로그래머스/lv0_한 번만 등장한 문자.js
+++ 프로그래머스/lv0_한 번만 등장한 문자.js
@@ -7,3 +7,16 @@ function solution(s) {
   }
   return arr.sort().join("");
 }
+function solution(s) {
+  let ans = [];
+
+  let sArr = s.split("");
+
+  sArr.forEach((item) => {
+    if (s.indexOf(item) === s.lastIndexOf(item)) {
+      ans.push(item);
+    }
+  });
+
+  return ans.sort().join("");
+}
