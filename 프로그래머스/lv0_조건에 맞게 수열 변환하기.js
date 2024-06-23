7f41244460e57fc6ec7294b92dd6b17debce20fa
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2024-06-23 19:29:40 +0900

    240623

---
+++ 프로그래머스/lv0_조건에 맞게 수열 변환하기.js
@@ -0,0 +1,12 @@
+function solution(arr, k) {
+  var answer = [];
+  for (const i of arr) {
+    if (k % 2 === 0) {
+      answer.push(i + k);
+    } else {
+      answer.push(i * k);
+    }
+  }
+
+  return answer;
+}
