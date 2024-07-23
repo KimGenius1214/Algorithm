c592b9bc2fd689dacaba7e98fc7f3ad40a7b044a
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2024-07-23 15:05:49 +0900

    240723

--- inflearn/section1/10. 문자 찾기.js
+++ inflearn/section1/10. 문자 찾기.js
@@ -6,5 +6,14 @@ function solution(s, t){
     return answer;
 }
 
+function solution(s, t){
+    let answer=s.split(t);
+    // for(const i of s){
+    //     if(i === t) answer++;
+    // }
+    
+    return answer.length - 1;
+}
+
 let str="COMPUTERPROGRAMMING";
 console.log(solution(str, 'R'));
