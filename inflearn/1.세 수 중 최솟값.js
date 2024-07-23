efca713627bbec549630c9fb1e4e5a16e1db5a51
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2024-07-23 11:23:54 +0900

    240723

---
+++ inflearn/1.세 수 중 최솟값.js
@@ -0,0 +1,17 @@
+// function solution(a, b, c){
+//   return Math.min(a,b,c);
+// }
+function solution(a,b,c){
+  let answer;
+  if(a < b){ 
+    answer = a;
+  }else {
+    answer = b;
+  }
+  if(c < answer){
+    answer = c;
+  }
+  return answer;
+}
+
+console.log(solution(2, 5, 1));
