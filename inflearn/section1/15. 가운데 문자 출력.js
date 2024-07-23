d957ff512fec36db3d0386de0ba28d8cdfa75449
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2024-07-23 15:48:53 +0900

    240723

--- inflearn/section1/15. 가운데 문자 출력.js
+++ inflearn/section1/15. 가운데 문자 출력.js
@@ -3,9 +3,11 @@ function solution(s){
     let len = s.length;
     let mid = Math.floor(len / 2)
     if(len % 2 === 0){
-        answer = s.substring(mid - 1, mid + 1)
+        // answer = s.substring(mid - 1, mid + 1)
+        answer = s.substr(mid - 1, 2)
     }else {
-        answer = s.substring(mid, mid + 1)
+        // answer = s.substring(mid, mid + 1)
+        answer = s.substr(mid, 1)
     }
     
     return answer;
