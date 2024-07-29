68029047f75cd54d1a8e31f3cc07fa7d9aeba0af
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2024-07-29 17:14:20 +0900

    240729

---
+++ inflearn/section4/5. K번쨰 큰 수.js
@@ -0,0 +1,17 @@
+function solution(n, k, card){
+    let answer;
+    let set = new Set();
+    for(let i = 0; i < n; i++){
+        for(let j = i + 1; j < n; j++){
+            for(let s = j + 1; s < n; s++){
+                set.add(card[i] + card[j] + card[s])
+            }
+        }
+    }    
+    let newArr = Array.from(set).sort((a, b)=> b - a);
+    answer = newArr[k - 1]
+    return answer;
+}
+
+let arr=[13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+console.log(solution(10, 3, arr));
