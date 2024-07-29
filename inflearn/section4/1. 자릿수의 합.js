f2a3b7ddcfd5e71d4699ea4d36219fdbee312575
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2024-07-29 12:32:17 +0900

    240729

--- inflearn/section4/1. 자릿수의 합.js
+++ inflearn/section4/1. 자릿수의 합.js
@@ -18,5 +18,25 @@ function solution(n, arr){
     return answer;
 }
 
+function solution(n, arr){
+    let answer;
+    let max = 0;
+    for(const i of arr){
+        let sum = 0;
+        let temp = i;
+        while(temp){
+            sum += (temp % 10);
+            temp = Math.floor(temp / 10);
+        }
+        if(sum > max){
+            max = sum;
+            answer = i;
+        }else if(sum === max){
+            if(i > answer) answer = i;
+        }
+    }
+    return answer;
+}
+
 let arr=[128, 460, 603, 40, 521, 138, 99];
 console.log(solution(7, arr));
