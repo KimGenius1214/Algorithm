7980a79fd9badd2fbcb81e6c88252f2e2a74604a
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2024-07-30 11:14:10 +0900

    240730

--- inflearn/section4/5. K번쨰 큰 수.js
+++ inflearn/section4/5. K번쨰 큰 수.js
@@ -6,7 +6,7 @@ function solution(n, k, card){
         // n - 1 까지 돌아도 됨
         for(let j = i + 1; j < n; j++){
             for(let s = j + 1; s < n; s++){
-                set.add(card[i] + card[j] + card[s])
+                set.add(card[i] + card[j] + card[s]);
             }
         }
     }    
