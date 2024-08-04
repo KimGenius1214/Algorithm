8276e15efa82b37bbd19fed2f58d0773a22eb804
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2024-08-04 22:09:18 +0900

    240804

--- inflearn/remind/5-8. 모든 아나그램 찾기.js
+++ inflearn/remind/5-8. 모든 아나그램 찾기.js
@@ -8,23 +8,20 @@ function compareMaps(map1, map2) {
 
 function solution(s, t){
     let answer = 0;
-    let len = t.length - 1;
-    let lt = 0;
     let sh = new Map();
     let th = new Map();
-
     for(const i of t){
         if(th.has(i)){
             th.set(i, th.get(i) + 1);
         }else th.set(i, 1);
     }
-
+    let len = t.length - 1;
     for(let i = 0; i < len; i++){
         if(sh.has(s[i])){
             sh.set(s[i], sh.get(s[i]) + 1);
         }else sh.set(s[i], 1);
     }
-
+    let lt = 0;
     for(let rt = 0; rt < s.length; rt++){
         if(sh.has(s[rt])) sh.set(s[rt], sh.get(s[rt]) + 1);
         else sh.set(s[rt], 1);
