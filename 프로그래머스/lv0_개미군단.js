72a94ccc24275e692a2d370ce7ff84e9bacd008c
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2024-08-10 20:25:48 +0900

    240810

---
+++ 프로그래머스/lv0_개미군단.js
@@ -0,0 +1,16 @@
+function solution(hp) {
+    var answer = 0;
+    while(hp > 0){
+        if(hp >= 5){
+            hp -= 5;
+            answer++;
+        }else if (hp >= 3){
+            hp -= 3;
+            answer++;
+        }else {
+            hp--;
+            answer++;
+        }
+    }
+    return answer;
+}
