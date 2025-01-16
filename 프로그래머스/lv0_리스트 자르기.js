4697e256b2fc9228c052216ae07242317aa46585
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2025-01-16 22:44:57 +0900

    250116(2)

--- 프로그래머스/lv0_리스트 자르기.js
+++ 프로그래머스/lv0_리스트 자르기.js
@@ -11,3 +11,22 @@ function solution(n, slicer, num_list) {
     }
     return answer;
 }
+
+function solution(n, slicer, num_list) {
+    let [a ,b, c] = slicer;
+    var answer = [];
+    switch(n) {
+        case 1:
+            answer = num_list.slice(0, b + 1);
+            break;
+        case 2:
+                answer = num_list.slice(a);
+            break;
+        case 3:
+                            answer = num_list.slice(a, b + 1);
+            break;
+        case 4:
+                answer = num_list.slice(a, b + 1).filter((r, i) => i % c === 0);
+    }
+    return answer;
+}
