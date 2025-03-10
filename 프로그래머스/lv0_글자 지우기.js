efd1ed32bd78df945391bf5b812e94569151b2ce
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2025-03-10 22:30:33 +0900

    250306

--- 프로그래머스/lv0_글자 지우기.js
+++ 프로그래머스/lv0_글자 지우기.js
@@ -1,9 +1,10 @@
 function solution(my_string, indices) {
-    var answer = '';
-    for(let i = 0; i <= my_string.length; i++){
-        if(!indices.includes(i)){
-            answer += my_string[i] ? my_string[i] : ''
-        }
+  var answer = "";
+  for (let i = 0; i < my_string.length; i++) {
+    if (!indices.includes(i)) {
+      answer += my_string[i];
     }
-    return answer;
+  }
+  return answer;
 }
+const arr = [];
