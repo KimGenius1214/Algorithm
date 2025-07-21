0e850a36d4a9c27c10597b75daeefd46d970e5db
youngjae kim <yj.kim@all-conec.com>
2025-07-21 13:00:41 +0900

    250721

--- 프로그래머스/lv0_무작위로 k개의 수 뽑기.js
+++ 프로그래머스/lv0_무작위로 k개의 수 뽑기.js
@@ -14,3 +14,13 @@ function solution(arr, k) {
   }
   return answer;
 }
+
+function solution(arr, k) {
+  let remove = new Set(arr);
+  let result = Array.from(remove).slice(0, k);
+
+  while (result.length < k) {
+    result.push(-1);
+  }
+  return result;
+}
