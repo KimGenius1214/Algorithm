f5c54ca4ee6483f8c43f695baa7bcc6d5ecb96bb
KimGenius1214 <dzdiz45@gmail.com>
2025-08-21 12:04:36 +0900

    250821

--- 프로그래머스/lv0_외계어 사전.js
+++ 프로그래머스/lv0_외계어 사전.js
@@ -1,15 +1,19 @@
 function solution(spell, dic) {
-    var answer = 2;
-    for(const i of dic){
-        let num = 0;
-        for(let j = 0; j < spell.length; j++){
-            if(i.includes(spell[j])){
-                num++;                
-            }
-        }
-        if(num === spell.length){
-            answer = 1;
-        }
+  var answer = 2;
+  for (const i of dic) {
+    let num = 0;
+    for (let j = 0; j < spell.length; j++) {
+      if (i.includes(spell[j])) {
+        num++;
+      }
     }
-    return answer;
+    if (num === spell.length) {
+      answer = 1;
+    }
+  }
+  return answer;
+}
+
+function solution(spell, dic) {
+  return dic.filter((d) => spell.every((s) => d.includes(s))).length ? 1 : 2;
 }
