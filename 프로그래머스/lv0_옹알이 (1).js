ea8ef3c2a980a4f4840ba57fea625aec2d6c34ec
KimGenius1214 <dzdiz45@gmail.com>
2025-08-22 12:29:16 +0900

    250822

--- 프로그래머스/lv0_옹알이 (1).js
+++ 프로그래머스/lv0_옹알이 (1).js
@@ -6,3 +6,19 @@ function solution(babbling) {
   });
   return answer;
 }
+function solution(babbling) {
+  var can = ["aya", "ye", "woo", "ma"];
+  var word = "";
+  var answer = 0;
+
+  for (var i = 0; i < babbling.length; i++) {
+    word = babbling[i].toString();
+    for (var j = 0; j < can.length; j++) {
+      word = word.replace(can[j], " ");
+    }
+    if (word.trim().length == 0) {
+      answer++;
+    }
+  }
+  return answer;
+}
