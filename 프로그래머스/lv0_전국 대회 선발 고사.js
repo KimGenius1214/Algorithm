6bec548dc7a8c291983d5ba1852c62dc4c0c0cbf
KimGenius1214 <dzdiz45@gmail.com>
2025-09-01 12:19:18 +0900

    250901

--- 프로그래머스/lv0_전국 대회 선발 고사.js
+++ 프로그래머스/lv0_전국 대회 선발 고사.js
@@ -11,3 +11,11 @@ function solution(rank, attendance) {
     rank.indexOf(arr[2]);
   return answer;
 }
+
+function solution(rank, attendance) {
+  const [a, b, c] = rank
+    .map((r, i) => [r, i])
+    .filter(([_, i]) => attendance[i])
+    .sort(([a], [b]) => a - b);
+  return 10000 * a[1] + 100 * b[1] + c[1];
+}
