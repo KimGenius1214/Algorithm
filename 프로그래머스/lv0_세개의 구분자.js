52c3faeb9aada65c42bfef69bc185b9f9284f999
KimGenius1214 <dzdiz45@gmail.com>
2025-09-02 11:52:52 +0900

    250902

--- 프로그래머스/lv0_세개의 구분자.js
+++ 프로그래머스/lv0_세개의 구분자.js
@@ -1,3 +1,13 @@
 function solution(myStr) {
   return myStr.match(/[^a-c]+/g) || ["EMPTY"];
 }
+
+function solution(myStr) {
+  var answer = [];
+  answer = [...myStr]
+    .map((e) => (e === "a" || e === "b" || e === "c" ? " " : e))
+    .join("")
+    .split(" ")
+    .filter((a) => a);
+  return answer.length ? answer : ["EMPTY"];
+}
