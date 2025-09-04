b0d113edcfb8497dbc3850ea9f8db0727208a24b
KimGenius1214 <dzdiz45@gmail.com>
2025-09-04 11:59:08 +0900

    250904

--- 프로그래머스/lv0_특별한 이차원 배열 1.js
+++ 프로그래머스/lv0_특별한 이차원 배열 1.js
@@ -6,3 +6,9 @@ function solution(n) {
   }
   return answer;
 }
+
+function solution(n) {
+  return Array.from({ length: n }, (_, i) =>
+    Array.from({ length: n }, (_, j) => (i === j ? 1 : 0))
+  );
+}
