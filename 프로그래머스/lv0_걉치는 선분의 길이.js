59cc08dda8c437b402e8a872afeb6c70f8e032fd
KimGenius1214 <dzdiz45@gmail.com>
2025-09-16 12:18:44 +0900

    250916

--- 프로그래머스/lv0_걉치는 선분의 길이.js
+++ 프로그래머스/lv0_걉치는 선분의 길이.js
@@ -13,14 +13,14 @@
 // }
 
 function solution(lines) {
-  const arr = [];
-  for (let i = 0; i < lines.length - 1; i++) {
-    if (lines[i][1] > lines[i + 1][0] && lines[i][1] < lines[i + 1][1]) {
-      for (let j = lines[i + 1][0]; j < lines[i][1]; j++) {
-        if (!arr.includes(j)) arr.push(j);
-      }
+  const start = Math.min(...lines.flat());
+  const end = Math.max(...lines.flat());
+  const lst = [...Array(end - start)].fill(0);
+
+  lines.forEach((el) => {
+    for (let i = el[0]; i < el[1]; i++) {
+      lst[i - start] += 1;
     }
-  }
-  console.log(arr);
-  return arr.length;
+  });
+  return lst.reduce((a, c) => (c > 1 ? a + 1 : a), 0);
 }
