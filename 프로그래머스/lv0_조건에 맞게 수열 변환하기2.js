c58478fee1f1d69907c5074d8126c45c7a3cb09e
KimGenius1214 <dzdiz45@gmail.com>
2025-09-23 12:09:10 +0900

    250923

--- 프로그래머스/lv0_조건에 맞게 수열 변환하기2.js
+++ 프로그래머스/lv0_조건에 맞게 수열 변환하기2.js
@@ -9,14 +9,11 @@ function solution(arr) {
       if (v >= 50 && v % 2 === 0) {
         return v / 2;
       }
-
       if (v < 50 && v % 2 === 1) {
         return v * 2 + 1;
       }
-
       return v;
     });
-
     for (let i = 0; i < arr.length; i++) {
       if (nextArr[i] !== arr[i]) {
         flag = true;
