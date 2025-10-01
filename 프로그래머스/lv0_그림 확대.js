55a28a88c6f8ec3ee520d3e3a69e75b84fb5c4a8
KimGenius1214 <dzdiz45@gmail.com>
2025-10-01 12:14:08 +0900

    251001

--- 프로그래머스/lv0_그림 확대.js
+++ 프로그래머스/lv0_그림 확대.js
@@ -15,3 +15,14 @@ function solution(picture, k) {
   }
   return answer.flat();
 }
+
+function solution(picture, k) {
+  let result = [];
+
+  picture.map((v) => {
+    const cur = [...v].map((c) => c.repeat(k)).join("");
+    for (let i = 0; i < k; i++) result.push(cur);
+  });
+
+  return result;
+}
