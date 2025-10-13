0066c36e10fd1adb0f916616e292b7373b17e535
KimGenius1214 <dzdiz45@gmail.com>
2025-10-13 12:20:20 +0900

    251013

--- 프로그래머스/lv0_수열과 구간 쿼리 2.js
+++ 프로그래머스/lv0_수열과 구간 쿼리 2.js
@@ -12,3 +12,12 @@ function solution(arr, queries) {
 
   return answer;
 }
+function solution(arr, queries) {
+  return queries.map(
+    ([s, e, k]) =>
+      arr
+        .slice(s, e + 1)
+        .filter((n) => n > k)
+        .sort((a, b) => a - b)[0] || -1
+  );
+}
