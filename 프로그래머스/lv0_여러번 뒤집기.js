3d693d3cad966ac7a190c66d41e5113e322e70c2
KimGenius1214 <dzdiz45@gmail.com>
2025-10-17 12:08:26 +0900

    251017

---
+++ 프로그래머스/lv0_여러번 뒤집기.js
@@ -0,0 +1,11 @@
+function solution(my_string, queries) {
+  var answer = my_string.split("");
+  let result = "";
+  for (const i of queries) {
+    let num = i[1] - i[0];
+    result = answer.slice(i[0], i[1] + 1).reverse();
+    answer.splice(i[0], num + 1);
+    answer.splice(i[0], 0, ...result);
+  }
+  return answer.join("");
+}
