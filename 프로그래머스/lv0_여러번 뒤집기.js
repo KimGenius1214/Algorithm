edb62f95a6d951e27e46b079f4741e288c63f22d
KimGenius1214 <dzdiz45@gmail.com>
2025-10-17 12:14:50 +0900

    251017

--- 프로그래머스/lv0_여러번 뒤집기.js
+++ 프로그래머스/lv0_여러번 뒤집기.js
@@ -9,3 +9,12 @@ function solution(my_string, queries) {
   }
   return answer.join("");
 }
+
+function solution(my_string, queries) {
+  let str = my_string.split("");
+  queries.forEach(([start, end]) => {
+    const changeStr = str.slice(start, end + 1);
+    str.splice(start, changeStr.length, ...changeStr.reverse());
+  });
+  return str.join("");
+}
