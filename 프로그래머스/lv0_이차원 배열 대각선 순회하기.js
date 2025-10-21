40c02dae3ad843e4d1c6dfbabd40e4f0db1f2f29
KimGenius1214 <dzdiz45@gmail.com>
2025-10-21 11:54:16 +0900

    251021

--- 프로그래머스/lv0_이차원 배열 대각선 순회하기.js
+++ 프로그래머스/lv0_이차원 배열 대각선 순회하기.js
@@ -9,3 +9,11 @@ function solution(board, k) {
   }
   return answer;
 }
+
+function solution(board, k) {
+  return board.reduce(
+    (total, row, i) =>
+      total + row.reduce((prev, num, j) => (i + j <= k ? prev + num : prev), 0),
+    0
+  );
+}
