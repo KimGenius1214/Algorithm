523f4d9735cdb0af6485d9b9fba843c747878f48
KimGenius1214 <dzdiz45@gmail.com>
2025-10-27 15:33:08 +0900

    251027

---
+++ 프로그래머스/lv0_유한소수 판별하기.js
@@ -0,0 +1,33 @@
+function solution(a, b) {
+  const getPrime = (num) => {
+    const prime = [];
+    for (let i = 2; i <= num; i++) {
+      while (num % i === 0) {
+        num = num / i;
+        prime.push(i);
+      }
+    }
+    return [...new Set(prime)];
+  };
+
+  const gcd = (num1, num2) => {
+    return num1 % num2 === 0 ? num2 : gcd(num2, num1 % num2);
+  };
+
+  const isInfinity = (num) => {
+    let isTrue = true;
+    for (const i of num) {
+      if (i !== 2 && i !== 5) {
+        isTrue = false;
+      }
+    }
+    return isTrue ? 1 : 2;
+  };
+
+  const common = gcd(a, b);
+  const denom = b / common;
+
+  var answer = isInfinity(getPrime(denom));
+
+  return answer;
+}
