a054adc37d28bc60408dbf2618406b6c89f3a9d3
KimGenius1214 <dzdiz45@gmail.com>
2025-10-28 16:40:50 +0900

    251028

--- 프로그래머스/lv0_유한소수 판별하기.js
+++ 프로그래머스/lv0_유한소수 판별하기.js
@@ -48,3 +48,11 @@ function solution(a, b) {
 
   return b === 1 ? 1 : 2;
 }
+
+function solution(a, b) {
+  let tmpB = b;
+  while (tmpB % 2 === 0) tmpB /= 2;
+  while (tmpB % 5 === 0) tmpB /= 5;
+  if (a % tmpB === 0) tmpB = 1;
+  return tmpB === 1 ? 1 : 2;
+}
