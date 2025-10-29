54bb92e93f907a8f05a54d31637b00ace0c05e4c
KimGenius1214 <dzdiz45@gmail.com>
2025-10-29 12:26:49 +0900

    251029

--- inflearn/2025/1-2. 삼각형 판별하기.js
+++ inflearn/2025/1-2. 삼각형 판별하기.js
@@ -1,14 +1,14 @@
 function solution(a, b, c) {
-  let max = 0;
+  let max = Math.max(a, b, c);
   const total = a + b + c;
-  if (a < b) {
-    max = b;
-  } else {
-    max = a;
-  }
-  if (c > max) {
-    max = c;
-  }
+  // if (a < b) {
+  //   max = b;
+  // } else {
+  //   max = a;
+  // }
+  // if (c > max) {
+  //   max = c;
+  // }
   return total - max <= max ? "NO" : "YES";
 }
 console.log(solution(13, 33, 17));
