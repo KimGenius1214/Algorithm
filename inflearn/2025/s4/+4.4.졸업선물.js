338bad32889dd564f2aa133327124268e8601592
KimGenius1214 <dzdiz45@gmail.com>
2025-11-04 13:20:48 +0900

    251104

--- inflearn/2025/s4/+4.4.졸업선물.js
+++ inflearn/2025/s4/+4.4.졸업선물.js
@@ -39,6 +39,44 @@ function solution(budget, gift) {
   return answer;
 }
 
+function solution(budget, arr) {
+  let answer = 0;
+  let n = arr.length;
+
+  budget.sort((a, b) => {
+    const totalA = a[0] + a[1];
+    const totalB = b[0] + b[1];
+    return totalA + totalB;
+  });
+
+  for (let discountIndex = 0; discountIndex < n; discountIndex++) {
+    let tempBudget = budget;
+    let count = 0;
+
+    let [price, delivery] = arr[discountIndex];
+    let discountPrice = price / 2 + delivery;
+
+    if (discountPrice > tempBudget) continue;
+
+    tempBudget -= discountPrice;
+    count++;
+
+    for (let i = 0; i < n; i++) {
+      if (discountIndex === i) continue;
+
+      let totalPrice = arr[i][0] + arr[i][1];
+
+      if (tempBudget >= totalPrice) {
+        tempBudget -= totalPrice;
+        count++;
+      }
+    }
+
+    answer = Math.max(answer, count);
+  }
+  return answer;
+}
+
 let arr = [
   [6, 6],
   [2, 2],
