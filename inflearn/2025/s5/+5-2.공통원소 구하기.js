913fa492b04a7efb78324c6655a027c8219cecc2
KimGenius1214 <dzdiz45@gmail.com>
2025-11-05 13:16:15 +0900

    251105

--- inflearn/2025/s5/+5-2.공통원소 구하기.js
+++ inflearn/2025/s5/+5-2.공통원소 구하기.js
@@ -21,6 +21,28 @@ function solution(arr1, arr2) {
   return answer;
 }
 
+function solution(arr1, arr2) {
+  let answer = [];
+  let n = arr1.length;
+  let m = arr2.length;
+
+  let p1 = 0;
+  let p2 = 0;
+
+  while (p1 < n && p2 < m) {
+    if (arr1[p1] === arr2[2]) {
+      answer.push(arr1[p1]);
+      p1++;
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) {
+      p1++;
+    } else {
+      p2++;
+    }
+  }
+  return answer;
+}
+
 let arr1 = [1, 3, 9, 5, 2];
 let arr2 = [3, 2, 5, 7, 8];
 
