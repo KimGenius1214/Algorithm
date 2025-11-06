41bea4258eb737e913085cbd514bf1a728ef14bd
KimGenius1214 <dzdiz45@gmail.com>
2025-11-06 13:41:41 +0900

    251106

--- inflearn/2025/s5.투포인터/+5-4. 연속부분수열2.js
+++ inflearn/2025/s5.투포인터/+5-4. 연속부분수열2.js
@@ -13,6 +13,23 @@ function solution(sum, arr) {
   return count;
 }
 
+function solution(m, arr) {
+  let count = 0;
+  let p1 = 0;
+  let sum = 0;
+
+  for (let p2 = 0; p2 < arr.length; p2++) {
+    sum += arr[p2++];
+
+    while (sum > m) {
+      sum -= arr[p1++];
+    }
+
+    count += p2 - p1 + 1;
+  }
+  return count;
+}
+
 let arr = [1, 3, 1, 2, 3];
 
 console.log(solution(5, arr));
