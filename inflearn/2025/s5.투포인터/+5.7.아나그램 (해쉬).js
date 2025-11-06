3b24bc1c086c2d41d6e03034290c2f8b698002cc
KimGenius1214 <dzdiz45@gmail.com>
2025-11-06 13:53:05 +0900

    251106

--- inflearn/2025/s5.투포인터/+5.7.아나그램 (해쉬).js
+++ inflearn/2025/s5.투포인터/+5.7.아나그램 (해쉬).js
@@ -1,3 +1,20 @@
+// function solution(str1, str2) {
+//   if (str1.length !== str2.length) return "NO";
+
+//   let map = new Map();
+//   for (const i of str1) {
+//     map.set(i, (map.get(i) || 0) + 1);
+//   }
+//   for (const i of str2) {
+//     if (!map.get(i) || map.get(i) === 0) {
+//       return "NO";
+//     }
+//     map.set(i, map.get(i) - 1);
+//   }
+
+//   return "YES";
+// }
+
 function solution(str1, str2) {
   if (str1.length !== str2.length) return "NO";
 
@@ -5,11 +22,15 @@ function solution(str1, str2) {
   for (const i of str1) {
     map.set(i, (map.get(i) || 0) + 1);
   }
+
   for (const i of str2) {
-    if (!map.get(i) || map.get(i) === 0) {
+    const count = map.get(i) || 0;
+
+    if (count === 0) {
       return "NO";
     }
-    map.set(i, map.get(i) - 1);
+
+    map.set(i, count - 1);
   }
 
   return "YES";
