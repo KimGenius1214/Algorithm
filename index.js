7d270ca7832383c17bf6487b0f31d8ca48bf042d
KimGenius1214 <dzdiz45@gmail.com>
2025-11-07 17:11:44 +0900

    251107

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 function solution(arr) {
   let n = arr.length;
 
-  for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
+  for (let i = 1; i < n; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[i] = arr[j];
+      j--;
     }
+    arr[j + 1] = key;
   }
 
   return arr;
