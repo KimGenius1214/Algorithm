b5dfaf29190a17d5c8deaa7cf3f1057b9037a11c
KimGenius1214 <dzdiz45@gmail.com>
2025-11-10 16:00:16 +0900

    251110

--- inflearn/2025/s4.브루투포스/+4.4.졸업선물.js
+++ inflearn/2025/s4.브루투포스/+4.4.졸업선물.js
@@ -77,6 +77,45 @@ function solution(budget, arr) {
   return answer;
 }
 
+function solution(budget, arr) {
+  let answer = 0;
+  let n = arr.length;
+  budget.sort((a, b) => {
+    const totalA = a[0] + a[1];
+    const totalB = b[0] + b[1];
+    return totalA - totalB;
+  });
+
+  for (let discountIndex = 0; discountIndex < n; discountIndex++) {
+    let tempBudget = budget;
+    let count = 0;
+
+    let [price, delivery] = budget[discountIndex];
+    let discountPrice = price / 2 + delivery;
+
+    if (tempBudget < discountPrice) {
+      continue;
+    }
+
+    tempBudget -= discountPrice;
+    count++;
+
+    for (let i = 0; i < n; i++) {
+      if (discountIndex === i) continue;
+
+      let totalPrice = arr[i][0] + arr[i][1];
+
+      if (totalPrice <= tempBudget) {
+        tempBudget -= totalPrice;
+        count++;
+      }
+    }
+    answer = Math.max(answer, count);
+  }
+
+  return answer;
+}
+
 let arr = [
   [6, 6],
   [2, 2],
