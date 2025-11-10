a52e88c48d82d18a02ab843e7265510bcb071c14
KimGenius1214 <dzdiz45@gmail.com>
2025-11-10 16:20:19 +0900

    251110

--- inflearn/2025/s5.투포인터/5-3.연속 부분 수열1.js
+++ inflearn/2025/s5.투포인터/5-3.연속 부분 수열1.js
@@ -17,6 +17,22 @@ function solution(sum, arr) {
   return count;
 }
 
+function solution(m, arr) {
+  let count = 0;
+  let p1 = 0;
+  let sum = 0;
+
+  for (let p2 = 0; p2 < arr.length; p2) {
+    sum += arr[p2];
+
+    while (sum > m) {
+      sum -= arr[p1++];
+    }
+
+    if (sum === m) count++;
+  }
+}
+
 let arr = [1, 2, 1, 3, 1, 1, 1, 2];
 
 console.log(solution(6, arr));
