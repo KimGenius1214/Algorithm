ade011085f3489a22d04fde4eca5139bdc05f5b2
KimGenius1214 <dzdiz45@gmail.com>
2025-11-10 16:10:51 +0900

    251110

--- inflearn/2025/s5.투포인터/+5-1.두배열 합치기.js
+++ inflearn/2025/s5.투포인터/+5-1.두배열 합치기.js
@@ -43,6 +43,28 @@ function solution(arr1, arr2) {
   return answer;
 }
 
+function solution(arr1, arr2) {
+  let answer = [];
+  let n = arr1.length;
+  let m = arr2.length;
+
+  let p1 = 0;
+  let p2 = 0;
+
+  while (n > p1 && m > p2) {
+    if (arr1[p1] <= arr2[p2]) {
+      answer.push(arr1[p1++]);
+    } else {
+      answer.push(arr2[p2++]);
+    }
+  }
+
+  while (p1 < n) answer.push(arr1[p1++]);
+  while (p2 < m) answer.push(arr2[p2++]);
+
+  return answer;
+}
+
 let arr1 = [1, 3, 5];
 let arr2 = [2, 3, 6, 7, 9];
 
