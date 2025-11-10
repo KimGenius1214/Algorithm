4688adf389ef584f12ceeaa30369dc3fc280a917
KimGenius1214 <dzdiz45@gmail.com>
2025-11-10 16:14:12 +0900

    251110

--- inflearn/2025/s5.투포인터/+5-2.공통원소 구하기.js
+++ inflearn/2025/s5.투포인터/+5-2.공통원소 구하기.js
@@ -45,6 +45,29 @@ function solution(arr1, arr2) {
   return answer;
 }
 
+function solution(arr1, arr2) {
+  let answer = [];
+  let n = arr1.length;
+  let m = arr2.length;
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+  let p1 = 0;
+  let p2 = 0;
+
+  while (n > p1 && m > p2) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1]);
+      p1++;
+      p2++;
+    } else if (arr1[p1] > arr2[p2]) {
+      p2++;
+    } else {
+      p1++;
+    }
+  }
+  return answer;
+}
+
 let arr1 = [1, 3, 9, 5, 2];
 let arr2 = [3, 2, 5, 7, 8];
 
