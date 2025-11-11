a74dae4c969d9fe16ef3235f320cdd9bcda86e1e
KimGenius1214 <dzdiz45@gmail.com>
2025-11-11 16:25:48 +0900

    251111

--- inflearn/2025/s7.정렬/+7-8.회의실 배정.js
+++ inflearn/2025/s7.정렬/+7-8.회의실 배정.js
@@ -12,6 +12,20 @@ function solution(arr) {
   return answer;
 }
 
+function solution(arr) {
+  arr.sort((a, b) => a[1] - b[1]);
+
+  let selected = [arr[0]];
+  let lastEnd = arr[0][1];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i][0] >= lastEnd) {
+      selected.push(arr[i]);
+      lastEnd = arr[i][1];
+    }
+  }
+}
+
 let arr = [
   [1, 4],
   [2, 3],
