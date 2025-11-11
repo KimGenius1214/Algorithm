21d86737983298c6a0231de7891cbf10e7fccf98
KimGenius1214 <dzdiz45@gmail.com>
2025-11-11 10:16:00 +0900

    251111

--- inflearn/2025/s6.자료구조/+6-5.쇠막대기.js
+++ inflearn/2025/s6.자료구조/+6-5.쇠막대기.js
@@ -29,6 +29,23 @@ function solution(str) {
   return answer;
 }
 
+function solution(str) {
+  let answer = 0;
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") stack.push("(");
+    else {
+      stack.pop();
+      if (str[i - 1] === ")") {
+        answer += stack.length;
+      } else {
+        answer++;
+      }
+    }
+  }
+}
+
 let str = "()(((()())(())()))(())";
 
 console.log(solution(str));
