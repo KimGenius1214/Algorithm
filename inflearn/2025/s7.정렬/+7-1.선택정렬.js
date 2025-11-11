4b5f7cd8eefd8208c0c20ac8dc4853be1c5f43f0
KimGenius1214 <dzdiz45@gmail.com>
2025-11-11 10:49:44 +0900

    251111

--- inflearn/2025/s7.정렬/+7-1.선택정렬.js
+++ inflearn/2025/s7.정렬/+7-1.선택정렬.js
@@ -19,6 +19,25 @@ function solution(arr) {
   return answer;
 }
 
+function solution(arr) {
+  let answer = [];
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let idx = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[idx]) {
+        idx = j;
+      }
+    }
+    if (i !== idx) {
+      [answer[idx], answer[i]] = [answer[i], answer[idx]];
+    }
+  }
+
+  return answer;
+}
+
 let arr = [13, 5, 11, 7, 23, 15];
 
 console.log(solution(arr));
