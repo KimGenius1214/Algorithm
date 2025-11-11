47c095c41fc64b2fd06a63afde6330acad936135
KimGenius1214 <dzdiz45@gmail.com>
2025-11-11 09:50:17 +0900

    251111

--- inflearn/2025/s6.자료구조/6-2.괄호문자제거.js
+++ inflearn/2025/s6.자료구조/6-2.괄호문자제거.js
@@ -16,5 +16,22 @@ function solution(str) {
   return answer;
 }
 
+function solution(str) {
+  let answer = "";
+  let stack = [];
+  for (const i of str) {
+    if (i === "(") {
+      stack.push(i);
+    } else if (i === ")") {
+      stack.pop();
+    } else {
+      if (stack.length === 0) {
+        answer += i;
+      }
+    }
+  }
+  return answer;
+}
+
 let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
 console.log(solution(str));
