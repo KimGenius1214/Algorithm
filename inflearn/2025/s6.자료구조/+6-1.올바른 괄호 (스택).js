8f9291981a077ecfa2e1a03f73bb159b1ddb66e0
KimGenius1214 <dzdiz45@gmail.com>
2025-11-11 09:43:07 +0900

    251111

--- inflearn/2025/s6.자료구조/+6-1.올바른 괄호 (스택).js
+++ inflearn/2025/s6.자료구조/+6-1.올바른 괄호 (스택).js
@@ -15,5 +15,18 @@ function solution(str) {
   return "YES";
 }
 
+function solution(str) {
+  let stack = [];
+  for (const i of str) {
+    if (i === "(") stack.push(i);
+    else {
+      if (stack.length === 0) return "NO";
+      else stack.pop();
+    }
+  }
+  if (stack.length > 0) return "NO";
+  return "YES";
+}
+
 let str = "(()(()))(()";
 console.log(solution(str));
