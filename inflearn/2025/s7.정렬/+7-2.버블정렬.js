611b4fb9ff24083a83b53d7257efc7151419f619
KimGenius1214 <dzdiz45@gmail.com>
2025-11-11 10:55:29 +0900

    251111

--- inflearn/2025/s7.정렬/+7-2.버블정렬.js
+++ inflearn/2025/s7.정렬/+7-2.버블정렬.js
@@ -12,6 +12,21 @@ function solution(arr) {
   return arr;
 }
 
+function solution(arr) {
+  let answer = [];
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = i; i < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+}
+
 let arr = [13, 5, 11, 7, 23, 15];
 
 console.log(solution(arr));
