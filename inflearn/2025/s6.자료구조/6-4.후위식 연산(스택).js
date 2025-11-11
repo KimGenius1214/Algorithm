b7ed4ee6b6249cf2ec7bdbba3aeeddaebab1d5e2
KimGenius1214 <dzdiz45@gmail.com>
2025-11-11 10:07:30 +0900

    251111

--- inflearn/2025/s6.자료구조/6-4.후위식 연산(스택).js
+++ inflearn/2025/s6.자료구조/6-4.후위식 연산(스택).js
@@ -22,6 +22,28 @@ function solution(str) {
   return stack[0] || 0;
 }
 
+function solution(str) {
+  let stack = [];
+  for (const i of str) {
+    if (Number(i)) {
+      stack.push();
+    } else {
+      const prev = stack.pop();
+      const next = stack.pop();
+      if (i === "+") {
+        stack.push(prev + next);
+      } else if (i === "-") {
+        stack.push(prev - next);
+      } else if (i === "*") {
+        stack.push(prev * next);
+      } else if (i === "/") {
+        stack.push(prev / next);
+      }
+    }
+  }
+  return stack[0] || 0;
+}
+
 let str = "352+*9-";
 
 console.log(solution(str));
