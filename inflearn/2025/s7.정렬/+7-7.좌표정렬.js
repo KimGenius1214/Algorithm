13a88363dbac015407efd888ebf807fd28fc88e9
KimGenius1214 <dzdiz45@gmail.com>
2025-11-11 15:55:11 +0900

    251111

--- inflearn/2025/s7.정렬/+7-7.좌표정렬.js
+++ inflearn/2025/s7.정렬/+7-7.좌표정렬.js
@@ -1,9 +1,5 @@
 function solution(arr) {
-  arr.sort((a, b) => {
-    if (a[0] === b[0]) return a[1] - b[1];
-    else return a[0] - b[0];
-  });
-  return arr;
+  arr.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]));
 }
 
 let arr = [
