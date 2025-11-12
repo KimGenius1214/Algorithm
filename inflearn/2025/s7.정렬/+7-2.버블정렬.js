f8fc092dd09108b6ee18b3b927c54baadc48a817
KimGenius1214 <dzdiz45@gmail.com>
2025-11-12 11:01:36 +0900

    251112

--- inflearn/2025/s7.정렬/+7-2.버블정렬.js
+++ inflearn/2025/s7.정렬/+7-2.버블정렬.js
@@ -27,6 +27,19 @@ function solution(arr) {
   return arr;
 }
 
+function solution(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - 1 - i; i++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+      }
+    }
+  }
+  return arr;
+}
+
 let arr = [13, 5, 11, 7, 23, 15];
 
 console.log(solution(arr));
