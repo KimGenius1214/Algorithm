c347e04cd25a617a501b6f6ff4705dd0198f3242
KimGenius1214 <dzdiz45@gmail.com>
2025-11-12 12:31:13 +0900

    251112

--- inflearn/2025/s7.정렬/+7-6.장난꾸러기 현수.js
+++ inflearn/2025/s7.정렬/+7-6.장난꾸러기 현수.js
@@ -10,6 +10,18 @@ function solution(arr) {
   return answer;
 }
 
+function solution(arr) {
+  let answer = [];
+  let sortedArr = [...arr];
+  sortedArr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== sortedArr[i]) answer.push(i);
+  }
+
+  return answer;
+}
+
 let arr = [120, 125, 152, 130, 135, 135, 143, 127, 160];
 
 console.log(solution(arr));
