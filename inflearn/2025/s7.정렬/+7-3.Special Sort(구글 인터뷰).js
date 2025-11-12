edf554d462716801df94608abe0d8614e8b84b32
KimGenius1214 <dzdiz45@gmail.com>
2025-11-12 11:04:23 +0900

    251112

--- inflearn/2025/s7.정렬/+7-3.Special Sort(구글 인터뷰).js
+++ inflearn/2025/s7.정렬/+7-3.Special Sort(구글 인터뷰).js
@@ -12,6 +12,18 @@ function solution(arr) {
   return arr;
 }
 
+function solution(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > 0 && arr[j + 1] < 0) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+}
+
 let arr = [1, 2, 3, -3, -2, 5, 6, -6];
 
 console.log(solution(arr));
