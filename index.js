585ac1f3d4dbdcf0d284cdd75a5f8aaa53952786
KimGenius1214 <dzdiz45@gmail.com>
2025-11-12 12:24:11 +0900

    251112

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-function solution(arr) {
-  arr.sort((a, b) => a[1] - b[1]);
-  let selected = [arr[0]];
-  let lastEnd = arr[0][1];
+function solution(s, arr) {
+  let answer = Array.from({ length: s }).fill(0);
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i][0] >= lastEnd) {
-      selected.push(arr[i]);
-      lastEnd = arr[i][1];
+  for (const i of arr) {
+    answer.unshift(i);
+    if (answer.length > s) {
+      answer.pop();
     }
   }
-  return selected.length;
+
+  return answer;
 }
 
-let arr = [
-  [1, 4],
-  [2, 3],
-  [3, 5],
-  [4, 6],
-  [5, 7],
-];
+let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
 
-console.log(solution(arr));
+console.log(solution(5, arr));
--- inflearn/2025/s7.정렬/+7-5.LRU.js
+++ inflearn/2025/s7.정렬/+7-5.LRU.js
@@ -18,6 +18,23 @@ function solution(s, arr) {
   return answer;
 }
 
+function solution(s, arr) {
+  let answer = Array.from({ length: s }).fill(0);
+
+  for (const i of arr) {
+    const index = answer.indexOf(i);
+    if (index !== -1) {
+      answer.splice(index, 1);
+    }
+    answer.unshift(i);
+    if (answer.length > s) {
+      answer.pop();
+    }
+  }
+
+  return answer;
+}
+
 let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
 
 console.log(solution(5, arr));
