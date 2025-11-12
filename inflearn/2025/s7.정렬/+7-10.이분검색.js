655abe1bfbb2eb6fd6e291089dc0aecee7b8cd12
KimGenius1214 <dzdiz45@gmail.com>
2025-11-12 14:55:20 +0900

    251112

--- inflearn/2025/s7.정렬/+7-10.이분검색.js
+++ inflearn/2025/s7.정렬/+7-10.이분검색.js
@@ -1,21 +1,18 @@
 function solution(m, arr) {
-  let left = 0;
-  let right = arr.length - 1;
-
+  let lt = 0;
+  let rt = arr.length - 1;
   arr.sort((a, b) => a - b);
 
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-
+  while (lt <= rt) {
+    let mid = Math.floor((lt + rt) / 2);
     if (arr[mid] === m) {
       return mid + 1;
     } else if (arr[mid] < m) {
-      left = mid + 1;
+      lt = mid + 1;
     } else {
-      right = mid - 1;
+      rt = mid - 1;
     }
   }
-
   return -1;
 }
 
