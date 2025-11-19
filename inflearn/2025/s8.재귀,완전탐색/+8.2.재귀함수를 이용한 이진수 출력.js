358874ecd3d50e83d5ac6c265d151330dec5692d
KimGenius1214 <dzdiz45@gmail.com>
2025-11-19 16:07:17 +0900

    251119

--- inflearn/2025/s8.재귀,완전탐색/+8.2.재귀함수를 이용한 이진수 출력.js
+++ inflearn/2025/s8.재귀,완전탐색/+8.2.재귀함수를 이용한 이진수 출력.js
@@ -10,3 +10,19 @@ function solution(n) {
   DFS(n);
   return answer;
 }
+
+function solution(n) {
+  let answer = "";
+  function DFS(n) {
+    if (n === 0) return;
+    else {
+      DFS(parseInt(n / 2));
+      answer += String(n % 2);
+    }
+  }
+  DFS(n);
+  return answer;
+}
+
+let n = 11;
+console.log(solution(n));
