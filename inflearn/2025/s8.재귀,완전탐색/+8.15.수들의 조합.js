9feb1185c619289da0dfeeb03537969da9182f6e
KimGenius1214 <dzdiz45@gmail.com>
2025-12-02 15:47:12 +0900

    251202

--- inflearn/2025/s8.재귀,완전탐색/+8.15.수들의 조합.js
+++ inflearn/2025/s8.재귀,완전탐색/+8.15.수들의 조합.js
@@ -16,5 +16,22 @@ function solution(n, k, arr, m) {
   DFS(0, 0, 0);
 }
 
+function solution(n, k, arr, m) {
+  let count = 0;
+
+  function DFS(level, start, sum) {
+    if (level === k) {
+      if (sum % m === 0) {
+        count++;
+      }
+      return;
+    }
+    for (let i = start; i < n; i++) {
+      DFS(level + 1, i + 1, sum + arr[i]);
+    }
+  }
+  DFS(0, 0, 0);
+}
+
 const arr = [2, 4, 5, 8, 12];
 console.log(solution(5, 3, arr, 6));
