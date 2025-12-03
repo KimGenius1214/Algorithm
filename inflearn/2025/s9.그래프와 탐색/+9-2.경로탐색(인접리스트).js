ce7bc2acae88c7122f6278e955cdd95599c57be2
KimGenius1214 <dzdiz45@gmail.com>
2025-12-03 16:23:56 +0900

    251203

--- inflearn/2025/s9.그래프와 탐색/+9-2.경로탐색(인접리스트).js
+++ inflearn/2025/s9.그래프와 탐색/+9-2.경로탐색(인접리스트).js
@@ -30,6 +30,38 @@ function solution(n, edges) {
   return count;
 }
 
+function solution(n, edges) {
+  const graph = Array.from({ length: n + 1 }, () => []);
+
+  for (let [from, to] of edges) {
+    graph[from].push(to);
+  }
+
+  const visited = Array(n + 1).fill(false);
+
+  let count = 0;
+
+  function DFS(current) {
+    if (current === n) {
+      count++;
+      return;
+    }
+
+    for (let next of graph[current]) {
+      if (!visited[current]) {
+        visited[next] = true;
+        DFS(next);
+        visited[next] = false;
+      }
+    }
+  }
+
+  visited[1] = true;
+  DFS(1);
+
+  return count;
+}
+
 const n = 5;
 const edges = [
   [1, 2],
