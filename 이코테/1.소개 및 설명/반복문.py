f9c4b7edbf45b4ce9d74572f9192217d6136fe57
KimGenius1214 <dzdiz45@gmail.com>
2025-12-04 14:31:52 +0900

    251204

---
+++ 이코테/1.소개 및 설명/반복문.py
@@ -0,0 +1,11 @@
+# 특정 소스코드 반복적으로 실행
+i = 1
+while i <= 9:
+    i += 1
+
+arr = [1, 3, 5]
+for i in arr:
+    print(i)
+# range() 연속적인 값을 차례대로 순회 ,range(시작값, 끝값) 인자를 하나만 넣으면 시작 값 = 0
+
+#continue , break 사용 가능
