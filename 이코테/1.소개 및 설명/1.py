838174eb8aa2ebb5a735155eeaff93f5a316347a
KimGenius1214 <dzdiz45@gmail.com>
2025-12-04 13:22:42 +0900

    251204

--- 이코테/1.소개 및 설명/1.py
+++ 이코테/1.소개 및 설명/1.py
@@ -3,3 +3,18 @@ start_time = time.time()
 
 end_time = time.time();
 print("time: " , end_time - start_time)
+
+#
+a = 1000
+b = -7
+
+a = 5.
+b = 156.93
+
+a  = 1e9  # 10의 9제곱
+
+# 752.5
+a = 75.25e1
+# 3.954
+b = 3954e-3
+
