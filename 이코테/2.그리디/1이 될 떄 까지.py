38a594bc94c2173d2f66aa718869009875f02dc9
KimGenius1214 <dzdiz45@gmail.com>
2025-12-05 14:10:30 +0900

    251205

--- 이코테/2.그리디/1이 될 떄 까지.py
+++ 이코테/2.그리디/1이 될 떄 까지.py
@@ -1,13 +1,30 @@
 n, k = map(int, input().split())
 
-count = 0
+# count = 0
 
-while(n != 1):
-    if(n % k == 0):
-        n //= k
-        count += 1
-    else:
-        n -= 1
-        count += 1
+# while(n != 1):
+#     if(n % k == 0):
+#         n //= k
+#         count += 1
+#     else:
+#         n -= 1
+#         count += 1
 
-print(count)
+# print(count)
+
+
+result = 0
+
+while True:
+    target = (n // k) * k
+    result = (n - target)
+    n = target
+
+    if(n < k):
+        break
+
+    result += 1
+    n //= k
+
+result += (n - 1)
+print(result)
