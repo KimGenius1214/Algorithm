c912a018ebe257d8b01a90c045168c95abbfd41d
KimGenius1214 <dzdiz45@gmail.com>
2025-12-05 16:31:38 +0900

    251205

---
+++ 프로그래머스/lv1_체육복.py
@@ -0,0 +1,21 @@
+def solution(n, lost, reserve):
+    interSec = list(set(lost).intersection(reserve))
+    lost = [i for i in lost if i not in interSec]
+    reserve = [i for i in reserve if i not in interSec]
+    count = 0    
+    
+    lost.sort()
+    reserve.sort()
+    
+    for i in lost:
+        front = i - 1
+        back = i + 1
+        
+        if(front in reserve):
+            count += 1
+            reserve.remove(front)
+        elif(back in reserve):
+            count +=1
+            reserve.remove(back)
+    answer = n - len(lost) + count
+    return answer
