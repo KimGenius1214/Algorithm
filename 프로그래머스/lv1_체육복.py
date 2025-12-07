6aff1244edf7867ca1925346e47bc0cd4fde6d3d
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2025-12-07 22:23:25 +0900

    251207

--- 프로그래머스/lv1_체육복.py
+++ 프로그래머스/lv1_체육복.py
@@ -19,3 +19,24 @@ def solution(n, lost, reserve):
             reserve.remove(back)
     answer = n - len(lost) + count
     return answer
+
+
+    def solution(n, lost, reserve):
+    interSec = list(set(lost).intersection(reserve))
+    lost = [i for i in lost if i not in reserve]
+    reserve = [i for i in reserve if i not in lost]
+    count = 0
+    lost.sort()
+    reserve.sort()
+
+    for i in lost:
+        front = i - 1
+        back = i + 1
+        if(front in reserve):
+            count += 1
+            reserve.remove(front)
+        elif(back in reserve):
+            count += 1
+            reserve.remove(back)
+    answer = n - len(lost) + count
+    return answer
