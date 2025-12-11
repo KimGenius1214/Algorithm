6187e2a359090f6e490f4f5855166999bcc52ff2
KimGenius1214 <dzdiz45@gmail.com>
2025-12-11 16:33:39 +0900

    251211

--- 이코테/2.그리디&구현/시각.py
+++ 이코테/2.그리디&구현/시각.py
@@ -1,34 +1,28 @@
 n = int(input())
 
-# hour, mnu, sec, count = 0, 0, 0, 0
+hour, mnu, sec, count = 0, 0, 0, 0
 
-# while True:
-#     if(hour == n):
-#         break;
-#     sec += 1
+while True:
+    if(hour > n):
+        break;
 
-#     if(sec == 60):
-#         mnu += 1
-#         sec = 0
+    if(sec == 60):
+        mnu += 1
+        sec = 0
     
-#     if(mnu == 60):
-#         hour += 1
-#         mnu = 0
+    if(mnu == 60):
+        hour += 1
+        mnu = 0
 
-#     if(str(sec).count('3') > 0):
-#         count += 1
-#     if(str(mnu).count('3') > 0):
-#         count += 1
-#     if(str(hour).count('3') > 0):
-#         count += 1
+    if '3' in str(hour) + str(mnu) + str(sec):
+        count += 1
 
+    sec += 1
 
-count = 0
-
-for i in range(n + 1):
-    for j in range(60):
-        for k in range(60):
-            if '3' in str(i) + str(j) + str(k):
-                count += 1
+# for i in range(n + 1):
+#     for j in range(60):
+#         for k in range(60):
+#             if '3' in str(i) + str(j) + str(k):
+#                 count += 1
 print(count)
 
