174a8a3d5646c0b66d8bf17f05853d0f666104a9
KimGenius1214 <dzdiz45@gmail.com>
2025-12-11 15:53:33 +0900

    251211

--- 이코테/2.그리디&구현/상하좌우.py
+++ 이코테/2.그리디&구현/상하좌우.py
@@ -2,22 +2,34 @@ n = int(input())
 data = list(map(str, input()))
 
 coor = [1,1]
-isFirst = True
+# isFirst = True
+
+# for i in data:
+#     if(coor[0] + 1 > n or coor[1] + 1 > n): continue
+#     if(i == 'L'):
+#         right = coor[1] - 1
+#         if(right == 0): continue
+#         coor = [coor[0] , right]
+#     elif(i == 'R'):
+#         coor = [coor[0], (coor[1] + 1)]
+#     elif(i == 'U'):
+#         left = (coor[0] - 1)
+#         if(left == 0): continue
+#         coor = [left, coor[1]]
+#     elif(i == 'D'):
+#         coor = [(coor[0] + 1), coor[1]]
+#     isFirst = False
 
 for i in data:
-    if(coor[0] + 1 > n or coor[1] + 1 > n): continue
-    if(i == 'L'):
-        right = coor[1] - 1
-        if(right == 0): continue
-        coor = [coor[0] , right]
-    elif(i == 'R'):
-        coor = [coor[0], (coor[1] + 1)]
-    elif(i == 'U'):
-        left = (coor[0] - 1)
-        if(left == 0): continue
-        coor = [left, coor[1]]
-    elif(i == 'D'):
-        coor = [(coor[0] + 1), coor[1]]
-    isFirst = False
+    left = coor[0]
+    right = coor[1]
+    if(i == 'L' and right > 1):
+        coor[1] -= 1
+    elif(i == 'R' and right < n):
+        coor[1] += 1
+    elif(i == 'U' and left > 1):
+        coor[0] -= 1
+    elif(i == 'D' and left < n):
+        coor[0] += 1
 
 print(coor)
