c1b84cc66d006bcf474bff50fb17ed2af4c798f4
KimGenius1214 <dzdiz45@gmail.com>
2025-12-11 16:11:13 +0900

    251211

--- 이코테/2.그리디&구현/상하좌우.py
+++ 이코테/2.그리디&구현/상하좌우.py
@@ -1,10 +1,10 @@
 n = int(input())
-data = list(map(str, input()))
+datas = list(map(str, input()))
 
 coor = [1,1]
 # isFirst = True
 
-# for i in data:
+# for i in datas:
 #     if(coor[0] + 1 > n or coor[1] + 1 > n): continue
 #     if(i == 'L'):
 #         right = coor[1] - 1
@@ -20,16 +20,32 @@ coor = [1,1]
 #         coor = [(coor[0] + 1), coor[1]]
 #     isFirst = False
 
-for i in data:
-    left = coor[0]
-    right = coor[1]
-    if(i == 'L' and right > 1):
-        coor[1] -= 1
-    elif(i == 'R' and right < n):
-        coor[1] += 1
-    elif(i == 'U' and left > 1):
-        coor[0] -= 1
-    elif(i == 'D' and left < n):
-        coor[0] += 1
+# for i in datas:
+#     left = coor[0]
+#     right = coor[1]
+#     if(i == 'L' and right > 1):
+#         coor[1] -= 1
+#     elif(i == 'R' and right < n):
+#         coor[1] += 1
+#     elif(i == 'U' and left > 1):
+#         coor[0] -= 1
+#     elif(i == 'D' and left < n):
+#         coor[0] += 1
+# print(coor)
 
-print(coor)
+x , y = 1, 1
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_type = ['L', 'R', 'U', 'D']
+
+for data in datas:
+    nx, ny = 0, 0
+    for i in range(len(move_type)):
+        if(data == move_type[i]):
+            nx = x + dx[i]
+            ny = y + dy[i]
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny 
+
+print(x, y)
