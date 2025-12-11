6e955b6ac6bb150d40a947205b14ac668b614d87
KimGenius1214 <dzdiz45@gmail.com>
2025-12-11 15:51:11 +0900

    251211

--- 이코테/2.그리디&구현/상하좌우.py
+++ 이코테/2.그리디&구현/상하좌우.py
@@ -1,2 +1,23 @@
 n = int(input())
-data = list(map(int, input()))
+data = list(map(str, input()))
+
+coor = [1,1]
+isFirst = True
+
+for i in data:
+    if(coor[0] + 1 > n or coor[1] + 1 > n): continue
+    if(i == 'L'):
+        right = coor[1] - 1
+        if(right == 0): continue
+        coor = [coor[0] , right]
+    elif(i == 'R'):
+        coor = [coor[0], (coor[1] + 1)]
+    elif(i == 'U'):
+        left = (coor[0] - 1)
+        if(left == 0): continue
+        coor = [left, coor[1]]
+    elif(i == 'D'):
+        coor = [(coor[0] + 1), coor[1]]
+    isFirst = False
+
+print(coor)
