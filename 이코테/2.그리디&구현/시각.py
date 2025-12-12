a3f12387a3599436a837d2e84cc8a524a82fdcee
KimGenius1214 <dzdiz45@gmail.com>
2025-12-12 16:01:54 +0900

    251212

--- 이코테/2.그리디&구현/시각.py
+++ 이코테/2.그리디&구현/시각.py
@@ -24,5 +24,13 @@ while True:
 #         for k in range(60):
 #             if '3' in str(i) + str(j) + str(k):
 #                 count += 1
+
+
+for i in range(n +1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
 print(count)
 
