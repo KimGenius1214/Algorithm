b4aa277c51b48ae15a3bfe88072c720dd1426412
KimGenius1214 <dzdiz45@gmail.com>
2025-12-15 15:43:36 +0900

    251215

--- 이코테/2.그리디&구현/왕실의 나이트.py
+++ 이코테/2.그리디&구현/왕실의 나이트.py
@@ -6,6 +6,13 @@ steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
 
 result = 0
 
+# for step in steps:
+#     next_row = row + step[0]
+#     next_column = row + step[1]
+
+#     if next_column >= 1 and next_column <= 8 and next_row >= 1 and next_column <= 8:
+#         result += 1
+
 for step in steps:
     next_row = row + step[0]
     next_column = row + step[1]
