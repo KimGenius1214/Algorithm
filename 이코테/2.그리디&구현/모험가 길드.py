0dd259f9d15dbf309409824d6bc62514626487f4
김영재 <97489808+KimGenius1214@users.noreply.github.com>
2025-12-27 11:12:45 +0900

    251227

--- 이코테/2.그리디&구현/모험가 길드.py
+++ 이코테/2.그리디&구현/모험가 길드.py
@@ -11,4 +11,14 @@ for i in arr:
         result += 1
         count = 0
 
+
+result = 0
+count = 0
+
+for i in arr:
+    count += 1
+    if(count >= i):
+        result += 1
+        count = 0
+
 print(result)
